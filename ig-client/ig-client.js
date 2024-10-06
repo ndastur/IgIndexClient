414a2871c0ec588ff6a6bf4b5dccceab533f84b0
ndastur <neville@dastur.me.uk>
2024-10-06 11:13:49 +0100

    pause cron

--- ig-client/ig-client.js
+++ ig-client/ig-client.js
@@ -18,14 +18,42 @@ export const base64ToPlainPassword = (base64password) => {
     return Buffer.from(base64password || '', 'base64').toString('ascii');
 }
 
-export const setUseDemo = (isUseDemo) => {
-    useDemo = isUseDemo;
-    urlEndpoint = `https://${(useDemo?'demo-':'')}api.ig.com/gateway/deal`;
+export const IGRestRequest = async (path, query = '', method = 'get') => {
+  try {
+    const headers = {
+      "Content-Type": 'application/json; charset=UTF-8',
+      "Accept": 'application/json; charset=UTF-8',
+      "X-IG-API-KEY": API_KEY,
+      "cst": LoginTokens.cst,
+      "x-security-token": LoginTokens['x-security-token']
+    };
+
+    var res = await axios.request({
+      method: method,
+      url: urlEndpoint + `${path}/` + query,
+      headers: headers,
+      timeout: 8000
+      });
+
+    var json = await res.data;
+    logVerbose('Get IG REST json resp:');
+    logVerbose(json);
+    return json;
+  }
+  catch(e) {
+    console.error(e);
+  }
 }
 
+export const logout = async () {
+  await IGRestRequest('/session', '', 'delete');
+};
+
 // Login to platform
-export const LoginAsync = async (username, password, api_key) => {
+export const LoginAsync = async (username, password, api_key, demo = false) => {
     try {
+        useDemo = demo;
+        urlEndpoint = `https://${(useDemo?'demo-':'')}api.ig.com/gateway/deal`;
         API_KEY = api_key;
         
         const loginData = {
@@ -68,33 +96,10 @@ export const LoginAsync = async (username, password, api_key) => {
     
 }
 
-export const GetIGRest = async (path, query = '') => {
-  try {
-    const headers = {
-      "X-IG-API-KEY": API_KEY,
-      "cst": LoginTokens.cst,
-      "x-security-token": LoginTokens['x-security-token']
-    };
-
-    var res = await axios.get(urlEndpoint + `${path}/` + query, {
-      headers
-    });
-
-    var json = await res.data;
-    logVerbose('Get IG REST json resp:');
-    logVerbose(json);
-    return json;
-  }
-  catch(e) {
-    console.error(e);
-  }
-
-}
-
 export const GetSessionEncryptionKey = async () => {
   try {
   var path = '/session/encryptionKey';
-  var json = await GetIGRest(path);
+  var json = await IGRestRequest(path);
   }
   catch(e) {
     console.error(e);
@@ -104,7 +109,7 @@ export const GetSessionEncryptionKey = async () => {
 export const GetSentiment = async (symbol) => {
   try {
     var symbolQuery = (symbol.includes(',')) ? '?marketIds=' + encodeURIComponent(symbol) : symbol;
-    var json = await GetIGRest('/clientsentiment', symbolQuery);
+    var json = await IGRestRequest('/clientsentiment', symbolQuery);
     //console.log(json);
     return json;
   }
@@ -118,7 +123,7 @@ export const GetSentiment = async (symbol) => {
 export const GetMarketDetailEpic = async (epic) => {
   try {
     var symbolQuery = (symbol.includes(',')) ? '?epics=' + encodeURIComponent(epic) : epic;
-    var json = await GetIGRest('/markets', symbolQuery);
+    var json = await IGRestRequest('/markets', symbolQuery);
     //console.log(json);
     return json;
   }
@@ -130,7 +135,7 @@ export const GetMarketDetailEpic = async (epic) => {
 export const GetMarketIdFromEpic = async (epic) => {
   try {
     var symbolQuery = (epic.includes(',')) ? '?epics=' + encodeURIComponent(epic) : epic;
-    var json = await GetIGRest('/markets', symbolQuery);
+    var json = await IGRestRequest('/markets', symbolQuery);
     if (epic.includes(',')) {
       var marketIds = json.marketDetails.map(function (element) {
         return {
@@ -159,7 +164,7 @@ export const GetMarketIdFromEpic = async (epic) => {
 
 export const GetMarketIdsFromWatchlist = async (watchlistId) => {
   try {
-    var watchlistJson = await GetIGRest('/watchlists', watchlistId);
+    var watchlistJson = await IGRestRequest('/watchlists', watchlistId);
     var epics = watchlistJson.markets.map(function(e){
       return e.epic;
     });
